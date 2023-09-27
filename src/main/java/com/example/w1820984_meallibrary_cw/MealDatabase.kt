986402b44f8492668a8ff600b3622a2eeeac8137
Nipun Luca <136705161+Nipun-Luca@users.noreply.github.com>
2023-09-27 20:57:56 +0100

    Delete src directory

--- src/main/java/com/example/w1820984_meallibrary_cw/MealDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.w1820984_meallibrary_cw
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [Meal::class], version=2)
-abstract class MealDatabase: RoomDatabase() {
-    abstract fun mealDao(): MealDao
-}
